1b48fd714f1bd9d9557a8b90aa525f4c41a8f313
Saúl Cabrera <saulecabrera@gmail.com>
2022-12-09 16:33:00 -0500

    Fix all the things

--- crates/ruvy/src/main.rs
+++ crates/ruvy/src/main.rs
@@ -1,45 +1,44 @@
-use std::{os::raw::c_char, ffi::CString};
+use std::{os::raw::c_char, ffi::CString, io::Read};
 use ruvy_wasm_sys::{ruby_init, rb_eval_string_protect, rb_load_file};
-use std::env;
+use std::{io, env};
 
 const PRELUDE: &'static str = r#"
-  Module X
-    def x
+  module X
+    def self.x
 	1 + 1
     end
   end
 "#;
 
 fn main() {
-    unsafe {
-	let prog = CString::new("puts 1").unwrap();
-	let prog = prog.as_ptr() as *const c_char;
-	let mut state = 0i32;
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+	panic!("expected exactly two arguments");
+    }
 
+    let input = &args[0];
+    let program = &args[1];
+
+    unsafe {
+	let prog = CString::new(program.as_str()).unwrap();
+	let prog = prog.as_ptr() as *const c_char;
+	let state = 0i32;
 
 	rb_eval_string_protect(prog, state as *mut i32);
 
-	println!("state: {}", state);
+	assert!(state == 0);
     }
 }
-
-// fn load_prelude() {
-//     let mut state = 0i32;
-//     let cwd = env::current_dir().unwrap();
-//     let prelude_path = cwd.join("prelude").join("x.rb");
-//     let prog = CString::new(prelude_path.to_str().unwrap()).unwrap();
-//     let prog = prog.as_ptr() as *const c_char;
-
-
-//     unsafe {
-// 	rb_load_file(prog);
-//     }
-// }
-
 
 #[export_name = "wizer.initialize"]
 pub extern "C" fn init() {
     unsafe {
 	ruby_init();
+	let prelude = CString::new(PRELUDE).unwrap();
+	let prelude = prelude.as_ptr() as *const c_char;
+
+	let mut state = 0i32;
+	rb_eval_string_protect(prelude, state as *mut i32);
+	assert!(state == 0);
     }
 }
