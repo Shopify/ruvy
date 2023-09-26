b2b1272a2b1264aa1cb0c10b5e7430dd7aeb89c0
Jeff Charles <jeff.charles@shopify.com>
2023-09-26 11:59:58 -0400

    Use dedicated Path::new for cargo manifest env

--- crates/cli/build.rs
+++ crates/cli/build.rs
@@ -6,14 +6,12 @@ fn main() -> Result<()> {
     let destination = Path::new(&env::var("OUT_DIR")?).join("engine.wasm");
     let is_clippy = env::var("CARGO_CFG_FEATURE").is_ok_and(|v| v == "cargo-clippy");
     if is_clippy {
-        fs::write(destination, &[])?;
+        fs::write(destination, [])?;
         println!("cargo:warning=using stubbed engine.wasm for static analysis");
     } else {
         println!("cargo:rerun-if-changed=build.rs");
-        let cargo_manifest_dir = env::var("CARGO_MANIFEST_DIR")?;
-        let engine_path =
-            format!("{cargo_manifest_dir}/../../target/wasm32-wasi/release/core.wasm");
-        let engine_path = Path::new(&engine_path);
+        let engine_path = Path::new(&env::var("CARGO_MANIFEST_DIR")?)
+            .join("../../target/wasm32-wasi/release/core.wasm");
         println!("cargo:rerun-if-changed={}", engine_path.to_str().unwrap());
         fs::copy(&engine_path, &destination)?;
     }
