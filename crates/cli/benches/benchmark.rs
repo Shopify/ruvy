657bec8dd32b93a21182f8872fc005282f694b95
Jeff Charles <jeff.charles@shopify.com>
2023-10-02 16:26:14 -0400

    Delegate more logic to enum and inline a couple functions

--- crates/cli/benches/benchmark.rs
+++ crates/cli/benches/benchmark.rs
@@ -3,7 +3,7 @@ use std::{
     fmt::{self, Display},
     fs,
     path::{Path, PathBuf},
-    process::Command,
+    process::{Command, ExitStatus},
     str,
 };
 
@@ -74,66 +74,27 @@ impl Display for WasmCase {
 
 impl WasmCase {
     fn new(strategy: CompilationStrategy, entrypoint: Entrypoint) -> Result<WasmCase> {
-        let name = format!(
-            "{}-{}",
-            match &strategy {
-                CompilationStrategy::WasiVFSRubyWasm => "rubywasm",
-                CompilationStrategy::Ruvy(_) => "ruvy",
-            },
-            entrypoint.parent_dirname
-        );
-        let output_path = output_path_for_wasm(&name);
-        let exit_status = match &strategy {
-            CompilationStrategy::WasiVFSRubyWasm => {
-                let ruby_wasm = helpers::ruby_wasm()?;
-                let wasi_vfs = helpers::wasi_vfs()?;
-                Command::new(wasi_vfs)
-                    .arg("pack")
-                    .arg(&ruby_wasm)
-                    .arg("--mapdir")
-                    .arg(format!("/src::{}", entrypoint.parent_path))
-                    // Examples online show mapping the `/usr` directory, however this
-                    // breaks when using a `minimal` instead of `full` profile of
-                    // ruby.wasm, so we don't map that directory here.
-                    .arg("-o")
-                    .arg(output_path.as_os_str())
-                    .status()?
-            }
-            CompilationStrategy::Ruvy(preload) => {
-                let ruvy = env!("CARGO_BIN_EXE_ruvy");
-                let mut args = vec![entrypoint.path, OsStr::new("-o"), output_path.as_os_str()];
-                if let Some(preload) = &preload {
-                    args.push(OsStr::new("--preload"));
-                    args.push(preload.as_os_str());
-                }
-                Command::new(ruvy).args(args).status()?
-            }
-        };
-
+        let name = format!("{}-{}", strategy, entrypoint.parent_dirname);
+        let output_path = helpers::cargo_target_tmpdir()
+            .join(&name)
+            .with_extension("wasm");
+        let exit_status = strategy.compile_wasm(&output_path, &entrypoint)?;
         if !exit_status.success() {
             bail!("Failed to build Wasm module");
         }
 
+        let input_file = &entrypoint.input_path;
+        let input = if input_file.exists() {
+            fs::read(input_file)?
+        } else {
+            vec![]
+        };
+
         Ok(Self {
             name,
             wasm: fs::read(output_path)?,
-            wasi_args: match &strategy {
-                &CompilationStrategy::WasiVFSRubyWasm => vec![
-                    // Not passing `--disable-gems` results in output about `RubyGems`,
-                    // `error_highlight`, `did_you_mean`, and `syntax_suggest` not
-                    // being loaded. We don't want that and we don't use the gems
-                    // anyway, so I'm disabling them.
-                    // If we did not want to pass `--disable-gems`, we can use the
-                    // `full` profile build of ruby.wasm and map the `/usr` directory.
-                    "--disable-gems".into(),
-                    PathBuf::from("/src")
-                        .join(entrypoint.filename)
-                        .to_string_lossy()
-                        .to_string(),
-                ],
-                &CompilationStrategy::Ruvy(_) => vec![],
-            },
-            input: input(&entrypoint)?,
+            wasi_args: strategy.wasi_args(&entrypoint),
+            input,
         })
     }
 
@@ -157,6 +118,69 @@ impl WasmCase {
 enum CompilationStrategy {
     WasiVFSRubyWasm,
     Ruvy(Option<PathBuf>),
+}
+
+impl CompilationStrategy {
+    fn compile_wasm(&self, output_path: &Path, entrypoint: &Entrypoint) -> Result<ExitStatus> {
+        match self {
+            CompilationStrategy::WasiVFSRubyWasm => {
+                let ruby_wasm = helpers::ruby_wasm()?;
+                let wasi_vfs = helpers::wasi_vfs()?;
+                Ok(Command::new(wasi_vfs)
+                    .arg("pack")
+                    .arg(&ruby_wasm)
+                    .arg("--mapdir")
+                    .arg(format!("/src::{}", entrypoint.parent_path))
+                    // Examples online show mapping the `/usr` directory, however this
+                    // breaks when using a `minimal` instead of `full` profile of
+                    // ruby.wasm, so we don't map that directory here.
+                    .arg("-o")
+                    .arg(output_path.as_os_str())
+                    .status()?)
+            }
+            CompilationStrategy::Ruvy(preload) => {
+                let ruvy = env!("CARGO_BIN_EXE_ruvy");
+                let mut args = vec![entrypoint.path, OsStr::new("-o"), output_path.as_os_str()];
+                if let Some(preload) = &preload {
+                    args.push(OsStr::new("--preload"));
+                    args.push(preload.as_os_str());
+                }
+                Ok(Command::new(ruvy).args(args).status()?)
+            }
+        }
+    }
+
+    fn wasi_args(&self, entrypoint: &Entrypoint) -> Vec<String> {
+        match self {
+            CompilationStrategy::WasiVFSRubyWasm => vec![
+                // Not passing `--disable-gems` results in output about `RubyGems`,
+                // `error_highlight`, `did_you_mean`, and `syntax_suggest` not
+                // being loaded. We don't want that and we don't use the gems
+                // anyway, so I'm disabling them.
+                // If we did not want to pass `--disable-gems`, we can use the
+                // `full` profile build of ruby.wasm and map the `/usr` directory.
+                "--disable-gems".into(),
+                PathBuf::from("/src")
+                    .join(entrypoint.filename)
+                    .to_string_lossy()
+                    .to_string(),
+            ],
+            CompilationStrategy::Ruvy(_) => vec![],
+        }
+    }
+}
+
+impl Display for CompilationStrategy {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                CompilationStrategy::WasiVFSRubyWasm => "rubywasm",
+                CompilationStrategy::Ruvy(_) => "ruvy",
+            }
+        )
+    }
 }
 
 struct Entrypoint<'a> {
@@ -184,19 +208,4 @@ impl<'a> From<&'a str> for Entrypoint<'a> {
             path: value.as_os_str(),
         }
     }
-}
-
-fn output_path_for_wasm(test_case_name: &str) -> PathBuf {
-    helpers::cargo_target_tmpdir()
-        .join(test_case_name)
-        .with_extension("wasm")
-}
-
-fn input(entrypoint: &Entrypoint) -> Result<Vec<u8>> {
-    let input_file = &entrypoint.input_path;
-    Ok(if input_file.exists() {
-        fs::read(input_file)?
-    } else {
-        vec![]
-    })
 }
