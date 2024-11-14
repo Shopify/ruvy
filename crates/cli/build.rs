ffd6260eab1dcd6d8e6f4884bc410f461b4a6460
Jeffrey Charles <jeff.charles@shopify.com>
2024-11-13 19:32:13 -0500

    Remove overlong table indexes without wasm-opt (#99)

--- crates/cli/build.rs
+++ crates/cli/build.rs
@@ -13,17 +13,7 @@ fn main() -> Result<()> {
         let engine_path = Path::new(env!("CARGO_MANIFEST_DIR"))
             .join("../../target/wasm32-wasip1/release/core.wasm");
         println!("cargo:rerun-if-changed={}", engine_path.to_str().unwrap());
-        // TODO we're using this update the encoding of the 0 index for the
-        // table used by `call_indirect` from a multi-byte LEB encoding to a
-        // single byte encoding so Wizer doesn't fail saying reference types
-        // aren't supported. I tried disabling reference-types in
-        // https://github.com/Shopify/ruvy/pull/92 but that still resulted in
-        // the engine Wasm module using a multi-byte LEB encoding for the table
-        // index so we need to investigate why that setting the compiler flag
-        // doesn't work as expected.
-        wasm_opt::OptimizationOptions::new_opt_level_3() // Aggressively optimize for speed.
-            .shrink_level(wasm_opt::ShrinkLevel::Level0) // Don't optimize for size at the cost of performance.
-            .run(&engine_path, &destination)?;
+        fs::copy(engine_path, destination)?;
     }
     Ok(())
 }
